6e8d103e3bb680f008e5762ec02457562e431e74
Do Thai Hoang <thaihoangdo0512@gmail.com>
2022-11-29 23:52:59 +0700

    Fix ManualService

--- app/Services/Api/ManualService.php
+++ app/Services/Api/ManualService.php
@@ -24,12 +24,11 @@ class ManualService extends Service
             $limit = $request->query('limit');
             $company_id = $request->company_id;
             if ($mansion_id) {
-                $manual = Manual::where('mansion_id', $mansion_id)
+                $manual = Manual::where(function ($query) use ($mansion_id) {
+                    $query->where('mansion_id', $mansion_id)
+                        ->orWhereNull('mansion_id');
+                })
                     ->where('flag', 1)
-                    ->orwhere(function ($q) use ($company_id) {
-                        $q->whereNull('mansion_id')
-                            ->where('flag', 1)->where('company_id', $company_id);
-                    })
                     ->where('company_id', $company_id)
                     ->with('mansions')
                     ->orderBy('id', 'DESC')
@@ -37,14 +36,11 @@ class ManualService extends Service
 
                 return $manual;
             }
-            $manual = Manual::whereIn('mansion_id', $buildingAdminMansionID)
+            $manual = Manual::where(function ($query) use ($buildingAdminMansionID) {
+                $query->whereIn('mansion_id', $buildingAdminMansionID)
+                    ->orWhereNull('mansion_id');
+            })
                 ->where('flag', 1)
-                // ->orwhereNull('mansion_id')
-                ->orwhere(function ($q) use ($company_id) {
-                    $q->whereNull('mansion_id')
-                        ->where('flag', 1)
-                        ->where('company_id', $company_id);
-                })
                 ->where('company_id', $company_id)
                 ->with('mansions')
                 ->orderBy('id', 'DESC')
